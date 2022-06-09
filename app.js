0ff06f89983c52f10c5102d780c2f9a4261330d0
Luciano Giuliani <104324947+LuchoGiuliani@users.noreply.github.com>
2022-06-09 19:57:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 2020;
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath))
+app.listen(port, () => console.log('listening on port ' + port))
+app.listen(2022)
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
